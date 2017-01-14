f606aba5135311b09a22b50341f2a7ccf47d47d5
Nelida Velazquez <tolkiana@gmail.com>
2017-01-14 17:11:02 -0500

    Creates the ForecastTableViewCell

---
+++ SportsWeather/Cells/ForecastTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ForecastTableViewCell.swift
+//  SportsWeather
+//
+//  Created by Nelida Velazquez on 1/14/17.
+//  Copyright © 2017 Tolkiana. All rights reserved.
+//
+
+import UIKit
+
+class ForecastTableViewCell: UITableViewCell {
+    @IBOutlet var scrollView: UIScrollView!
+}
