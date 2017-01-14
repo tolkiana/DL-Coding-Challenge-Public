b4fd1c433f38824be70293fafc1d5a5e7dacd1c6
Nelida Velazquez <tolkiana@gmail.com>
2017-01-14 15:04:58 -0500

    Renames ViewController and adds basic UI elements

--- SportsWeather/ViewControllers/CurrentWeatherViewController.swift
+++ SportsWeather/ViewControllers/CurrentWeatherViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CurrentWeatherViewController.swift
 //  SportsWeather
 //
 //  Created by Nelida Velazquez on 1/14/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CurrentWeatherViewController: UIViewController {
 
 }
 
