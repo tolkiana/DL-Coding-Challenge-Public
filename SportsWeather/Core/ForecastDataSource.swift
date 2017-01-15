8af1a0a3383a1d6a81f3a9790a77de9abbfad96d
Nelida Velazquez <tolkiana@gmail.com>
2017-01-15 18:17:59 -0500

    Make function static to avoid instante the class

--- SportsWeather/Core/ForecastDataSource.swift
+++ SportsWeather/Core/ForecastDataSource.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct ForecastDataSource {
-    func hourlyForecast() -> ForecastGroupViewModel {
+    static func hourlyForecast() -> ForecastGroupViewModel {
         let hours = ["10:00","11:00","12:00", "13:00","14:00","15:00","15:00","16:00","17:00","18:00","19:00","20:00"]
         var hourlyForecast: [ForecastViewModel] = []
         var temp: Float = 50
@@ -20,7 +20,7 @@ struct ForecastDataSource {
         return ForecastGroupViewModel(title: "Today", forecasts: hourlyForecast)
     }
     
-    func dailyForecast() -> ForecastGroupViewModel{
+    static func dailyForecast() -> ForecastGroupViewModel{
         let days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
         var dailyForecast: [ForecastViewModel] = []
         var temp: Float = 62
