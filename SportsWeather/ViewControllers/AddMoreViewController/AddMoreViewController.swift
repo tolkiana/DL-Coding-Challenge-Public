ca716efeb7407042e2eb5be35d7fdba6bd809864
Nelida Velazquez <tolkiana@gmail.com>
2017-01-15 14:19:09 -0500

    Creates ViewController that shows more options

---
+++ SportsWeather/ViewControllers/AddMoreViewController/AddMoreViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddMoreViewController.swift
+//  SportsWeather
+//
+//  Created by Nelida Velazquez on 1/15/17.
+//  Copyright © 2017 Tolkiana. All rights reserved.
+//
+
+import UIKit
+
+class AddMoreViewController: UIViewController {
+
+    
+    // MARK: IBActions
+    
+    @IBAction func close(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
