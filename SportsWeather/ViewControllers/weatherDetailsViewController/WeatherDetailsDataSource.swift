34917cbc0729a8875ea9bd98011595dc6c05ea69
Nelida Velazquez <tolkiana@gmail.com>
2017-01-14 21:14:04 -0500

    Creates a dataSource and delegate for the weather details

---
+++ SportsWeather/ViewControllers/weatherDetailsViewController/WeatherDetailsDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherDetailsDataSource.swift
+//  SportsWeather
+//
+//  Created by Nelida Velazquez on 1/14/17.
+//  Copyright © 2017 Tolkiana. All rights reserved.
+//
+
+import UIKit
+
+class WeatherDetailsDataSource: NSObject {
+
+}
