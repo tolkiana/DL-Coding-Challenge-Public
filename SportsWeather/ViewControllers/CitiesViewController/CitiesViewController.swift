116ef7ec1e1b3f2ad787529049b457114c3fa505
Nelida Velazquez <tolkiana@gmail.com>
2017-01-16 00:24:14 -0500

    Moves UIPageViewControllerDataSource to an extension

--- SportsWeather/ViewControllers/CitiesViewController/CitiesViewController.swift
+++ SportsWeather/ViewControllers/CitiesViewController/CitiesViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CitiesViewController: UIPageViewController, UIPageViewControllerDataSource {
+class CitiesViewController: UIPageViewController {
 
     // MARK: View life cycle
     
@@ -30,49 +30,6 @@ class CitiesViewController: UIPageViewController, UIPageViewControllerDataSource
             animated: true,
             completion: nil
         )
-    }
-    
-    // MARK: UIPageViewControllerDataSource
-    
-    func presentationCount(for pageViewController: UIPageViewController) -> Int {
-        return self.conditionsViewControllers.count
-    }
-    
-    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
-        guard let firstViewController = viewControllers?.first,
-            let firstVireControllerIndex = self.conditionsViewControllers.index(of: firstViewController) else {
-                return 0
-        }
-        return firstVireControllerIndex
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        guard let viewControllerIndex = self.conditionsViewControllers.index(of: viewController) else {
-            return nil
-        }
-        let previousIndex = viewControllerIndex - 1
-        guard previousIndex >= 0 else {
-            return nil
-        }
-        guard self.conditionsViewControllers.count > previousIndex else {
-            return nil
-        }
-        return self.conditionsViewControllers[previousIndex]
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        guard let viewControllerIndex = self.conditionsViewControllers.index(of: viewController) else {
-            return nil
-        }
-        let nextIndex = viewControllerIndex + 1
-        let conditionsViewControllersCount = self.conditionsViewControllers.count
-        guard conditionsViewControllersCount != nextIndex else {
-            return nil
-        }
-        guard self.conditionsViewControllers.count > nextIndex else {
-            return nil
-        }
-        return self.conditionsViewControllers[nextIndex]
     }
     
     // MARK: Private
