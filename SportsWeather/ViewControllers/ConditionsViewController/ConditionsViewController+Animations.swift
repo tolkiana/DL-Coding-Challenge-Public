fcb9e699c47c2991c2e0ac1b4d2c7fcff2ec80a5
Nelida Velazquez <tolkiana@gmail.com>
2017-01-15 21:09:11 -0500

    Add sliding animation for the moreInfoView

--- SportsWeather/ViewControllers/ConditionsViewController/ConditionsViewController+Animations.swift
+++ SportsWeather/ViewControllers/ConditionsViewController/ConditionsViewController+Animations.swift
@@ -13,5 +13,24 @@ extension ConditionsViewController {
     func startAnimations() {
         self.cityName.slideInAnimation(duration: Constants.Animations.fadeInDuration, completion: nil)
         self.temperatureInfo.fadeInAnimation(duration: Constants.Animations.fadeInDuration, completion: nil)
+        self.moreInfoOutAnimation()
+    }
+    
+    // MARK: Private methods
+    
+    private func moreInfoOutAnimation() {
+        self.moreInfoView.slideOutAnimation(duration: Constants.Animations.slideOutMoreInfoDuration) {
+            self.moreInfoTitle.text = "Humidity"
+            self.moreInfoValue.text = "10%"
+            self.moreInfoInAnimation()
+        }
+    }
+    
+    private func moreInfoInAnimation() {
+        self.moreInfoView.slideInAnimation(duration: Constants.Animations.slideInDuration) {
+            self.moreInfoTitle.text = "Precipitation"
+            self.moreInfoValue.text = "5%"
+            self.moreInfoOutAnimation()
+        }
     }
 }
