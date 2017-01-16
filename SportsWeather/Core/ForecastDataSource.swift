0c424280799e5189d8a1932577186601846360cb
Nelida Velazquez <tolkiana@gmail.com>
2017-01-15 19:00:43 -0500

    Change format so the degrees show as a integer number

--- SportsWeather/Core/ForecastDataSource.swift
+++ SportsWeather/Core/ForecastDataSource.swift
@@ -10,11 +10,11 @@ import Foundation
 
 struct ForecastDataSource {
     static func hourlyForecast() -> ForecastGroupViewModel {
-        let hours = ["10:00","11:00","12:00", "13:00","14:00","15:00","15:00","16:00","17:00","18:00","19:00","20:00"]
+        let hours = ["10:00","11:00","12:00", "13:00","14:00","15:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00"]
         var hourlyForecast: [ForecastViewModel] = []
-        var temp: Float = 50
+        var temp = 50
         for hour in hours {
-            hourlyForecast.append(ForecastViewModel(title: hour, iconName: Constants.Images.sunny, temperature: temp))
+            hourlyForecast.append(ForecastViewModel(title: hour, iconName: Constants.Images.sunny, temperature: String(temp)))
             temp += 1
         }
         return ForecastGroupViewModel(title: "Today", forecasts: hourlyForecast)
@@ -23,9 +23,9 @@ struct ForecastDataSource {
     static func dailyForecast() -> ForecastGroupViewModel{
         let days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
         var dailyForecast: [ForecastViewModel] = []
-        var temp: Float = 62
+        var temp = 62
         for day in days {
-            dailyForecast.append(ForecastViewModel(title: day, iconName: Constants.Images.partlyCoudly, temperature: temp))
+            dailyForecast.append(ForecastViewModel(title: day, iconName: Constants.Images.partlyCoudly, temperature: String(temp)))
             temp += 1
         }
         return ForecastGroupViewModel(title: "Forecast", forecasts: dailyForecast)
