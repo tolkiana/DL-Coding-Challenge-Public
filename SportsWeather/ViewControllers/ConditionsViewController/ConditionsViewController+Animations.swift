4bfeb008db9d5850c5eb7cd73d3cacfe1ebe67b8
Nelida Velazquez <tolkiana@gmail.com>
2017-01-15 21:37:15 -0500

    Fixes moreInfoView animation

--- SportsWeather/ViewControllers/ConditionsViewController/ConditionsViewController+Animations.swift
+++ SportsWeather/ViewControllers/ConditionsViewController/ConditionsViewController+Animations.swift
@@ -13,24 +13,32 @@ extension ConditionsViewController {
     func startAnimations() {
         self.cityName.slideInAnimation(duration: Constants.Animations.fadeInDuration, completion: nil)
         self.temperatureInfo.fadeInAnimation(duration: Constants.Animations.fadeInDuration, completion: nil)
-        self.moreInfoOutAnimation()
+        DispatchQueue.main.asyncAfter(deadline: .now() + Constants.Animations.slideOutMoreInfoDelay) { [weak self] in
+            self?.moreInfoOutAnimation()
+        }
     }
     
     // MARK: Private methods
     
     private func moreInfoOutAnimation() {
+        
         self.moreInfoView.slideOutAnimation(duration: Constants.Animations.slideOutMoreInfoDuration) {
-            self.moreInfoTitle.text = "Humidity"
-            self.moreInfoValue.text = "10%"
+            if self.moreInfoTitle.text == "Humidity" {
+                self.moreInfoTitle.text = "Precipitation"
+                self.moreInfoValue.text = "5%"
+            } else {
+                self.moreInfoTitle.text = "Humidity"
+                self.moreInfoValue.text = "10%"
+            }
             self.moreInfoInAnimation()
         }
     }
     
     private func moreInfoInAnimation() {
-        self.moreInfoView.slideInAnimation(duration: Constants.Animations.slideInDuration) {
-            self.moreInfoTitle.text = "Precipitation"
-            self.moreInfoValue.text = "5%"
-            self.moreInfoOutAnimation()
+        self.moreInfoView.slideInAnimation(duration: Constants.Animations.slideInMoreInfoDuration) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Constants.Animations.slideOutMoreInfoDelay) { [weak self] in
+                self?.moreInfoOutAnimation()
+            }
         }
     }
 }
