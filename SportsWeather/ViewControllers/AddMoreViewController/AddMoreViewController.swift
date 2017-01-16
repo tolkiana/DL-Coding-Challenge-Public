af5f1b74f25b2aed2b9278bf90463c81ac732736
Nelida Velazquez <tolkiana@gmail.com>
2017-01-16 00:18:44 -0500

    Refactors AddMoreViewController to use an external dataSource

--- SportsWeather/ViewControllers/AddMoreViewController/AddMoreViewController.swift
+++ SportsWeather/ViewControllers/AddMoreViewController/AddMoreViewController.swift
@@ -8,48 +8,27 @@
 
 import UIKit
 
-class AddMoreViewController: UIViewController, UITableViewDataSource {
-
+class AddMoreViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
+    
+    private var dataSource = AddMoreDataSource()
+    
+    // MARK: View life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupTableView()
+    }
     
     // MARK: IBActions
     
     @IBAction func close(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
-
-    // MARK: UITableViewDataSource
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
+    // MARK: Private methods
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        switch indexPath.row {
-        case AddMoreViewControllerConstants.Rows.annArbor.rawValue:
-            return tableView.dequeueReusableCell(withIdentifier: AddMoreViewControllerConstants.CellsIdentifiers.annArborCell)!
-        case AddMoreViewControllerConstants.Rows.losAngeles.rawValue:
-            return tableView.dequeueReusableCell(withIdentifier: AddMoreViewControllerConstants.CellsIdentifiers.losAngelesCell)!
-        case AddMoreViewControllerConstants.Rows.seatle.rawValue:
-            return tableView.dequeueReusableCell(withIdentifier: AddMoreViewControllerConstants.CellsIdentifiers.seatleCell)!
-        case AddMoreViewControllerConstants.Rows.saltLakeCity.rawValue:
-            return tableView.dequeueReusableCell(withIdentifier: AddMoreViewControllerConstants.CellsIdentifiers.saltLakeCell)!
-        default:
-            return tableView.dequeueReusableCell(withIdentifier: AddMoreViewControllerConstants.CellsIdentifiers.annArborCell)!
-        }
-    }
-
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if (editingStyle == .delete) {
-            // Remove cell here,we won't do it just yet, this is just for demoing
-        }
+    private func setupTableView() {
+        self.tableView.dataSource = dataSource
     }
 }
